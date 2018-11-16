17e41bd15a3e5849adba2bc0c083cdf952e9ccaa
Lap Nguyen <tanlapnguyen91@gmail.com>
2018-11-16 16:18:40 +0700

    Init project

---
+++ src/main/java/LapNguyen8130/LapNguyen8130/App.java
@@ -0,0 +1,13 @@
+package LapNguyen8130.LapNguyen8130;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
